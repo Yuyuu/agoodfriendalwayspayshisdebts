3a42522b35f2fd60c2b1a934aa71703351b045a0
Vincent Tertre <vincent.tertre@gmail.com>
2015-07-09 20:51:41 +0200

    memory repository to ease testing

---
+++ memory.py
@@ -0,0 +1,25 @@
+from locator import RepositoryLocator
+
+
+class MemoryRepository():
+    def __init__(self):
+        self.entities = {}
+
+    def add(self, entity):
+        self.entities[entity.oid] = entity
+
+    def get(self, oid):
+        return self.entities[oid]
+
+
+class MemoryEventRepository(MemoryRepository):
+    pass
+
+
+class MemoryRepositoryLocator(RepositoryLocator):
+    def __init__(self):
+        RepositoryLocator.__init__(self)
+        self.memoryEventRepository = MemoryEventRepository()
+
+    def _get_events(self):
+        return self.memoryEventRepository
