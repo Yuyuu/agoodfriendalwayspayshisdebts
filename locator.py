a427b3d91a24e7aed0b82a7504d676ec4b75b1fc
Vincent Tertre <vincent.tertre@gmail.com>
2015-07-09 20:37:39 +0200

    add a repository locator to access the repositories

---
+++ locator.py
@@ -0,0 +1,30 @@
+import abc
+
+import repository
+
+
+collection = None
+
+
+class RepositoryLocator:
+    def __init__(self):
+        pass
+
+    instance = None
+
+    @staticmethod
+    def initialize(repository_locator):
+        RepositoryLocator.instance = repository_locator
+
+    @staticmethod
+    def events():
+        return RepositoryLocator.instance._get_events()
+
+    @abc.abstractmethod
+    def _get_events(self):
+        return
+
+
+class MongoRepositoryLocator(RepositoryLocator):
+    def _get_events(self):
+        return repository.EventRepository(collection)
