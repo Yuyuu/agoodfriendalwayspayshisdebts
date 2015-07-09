517ba4f38c23535143f2982bc6db36b6f23d1866
Vincent Tertre <vincent.tertre@gmail.com>
2015-07-09 10:03:06 +0200

    events domain

---
+++ events.py
@@ -0,0 +1,47 @@
+import uuid
+
+
+class Event:
+    def __init__(self, name, participants):
+        self.uuid = uuid.uuid4()
+        self.name = name
+        self.participants = participants
+        self.purchases = []
+
+    def add_purchase(self, purchase):
+        self.purchases.append(purchase)
+
+    def _get_serialized_purchases(self):
+        serialized_purchases = []
+        for purchase in self.purchases:
+            serialized_purchases.append(purchase.serialize())
+        return serialized_purchases
+
+    def serialize(self):
+        return {
+            'id': str(self.uuid),
+            'name': self.name,
+            'participants': self.participants,
+            'purchases': self._get_serialized_purchases()
+        }
+
+
+class Purchase:
+    def __init__(self, purchaser, title, amount):
+        self.purchaser = purchaser
+        self.title = title
+        self.amount = amount
+        self.participants = []
+        self.description = None
+
+    def add_participant(self, participant):
+        self.participants.append(participant)
+
+    def serialize(self):
+        return {
+            'purchaser': self.purchaser,
+            'title': self.title,
+            'amount': self.amount,
+            'participants': self.participants,
+            'description': self.description
+        }
---
+++ tests/events_test.py
@@ -0,0 +1,83 @@
+import unittest
+
+import events
+
+
+class PurchaseTestCase(unittest.TestCase):
+    def test_has_purchaser(self):
+        purchaser = 'Kim'
+        purchase = events.Purchase(purchaser, '', 1)
+        self.assertEqual(purchaser, purchase.purchaser)
+        
+    def test_has_title(self):
+        title = 'Shopping'
+        purchase = events.Purchase('', 'Shopping', 1)
+        self.assertEqual(title, purchase.title)
+        
+    def test_has_amount(self):
+        amount = 10.04
+        purchase = events.Purchase('', '', amount)
+        self.assertEqual(amount, purchase.amount)
+        
+    def test_has_no_participants_by_default(self):
+        purchase = events.Purchase('', '', 1)
+        self.assertEqual(len(purchase.participants), 0)
+
+    def test_has_no_description_by_default(self):
+        purchase = events.Purchase('', '', 1)
+        self.assertIsNone(purchase.description)
+
+    def test_can_add_a_participant(self):
+        purchase = events.Purchase('', '', 1)
+        purchase.add_participant('Bob')
+        self.assertListEqual(purchase.participants, ['Bob'])
+
+    def test_serialization(self):
+        purchase = events.Purchase('Kim', 'Shopping', 10.04)
+        purchase.add_participant('Bob')
+        expected_purchase = {
+            'purchaser': 'Kim',
+            'title': 'Shopping',
+            'amount': 10.04,
+            'participants': ['Bob'],
+            'description': None
+        }
+        self.assertDictEqual(expected_purchase, purchase.serialize())
+
+
+class EventTestCase(unittest.TestCase):
+    def test_generates_an_uuid_upon_creation(self):
+        event = events.Event('', [])
+        self.assertIsNotNone(event.uuid)
+
+    def test_has_name(self):
+        name = 'Cool event'
+        event = events.Event(name, [])
+        self.assertEqual(event.name, name)
+
+    def test_has_participants(self):
+        participants = ['Kim', 'Lea', 'Bob']
+        event = events.Event('', participants)
+        self.assertListEqual(event.participants, participants)
+
+    def test_has_no_purchases_at_start(self):
+        event = events.Event('', [])
+        self.assertEqual(len(event.purchases), 0)
+
+    def test_can_add_a_purchase(self):
+        purchase = events.Purchase('Kim', 'Shopping', 2)
+        event = events.Event('', [])
+        event.add_purchase(purchase)
+        self.assertListEqual(event.purchases, [purchase])
+
+    def test_serialization(self):
+        event = events.Event('Cool event', ['Bob', 'Kim'])
+        purchase = events.Purchase('Bob', 'Gas', 10)
+        event.add_purchase(purchase)
+        expected_event = {
+            'id': str(event.uuid),
+            'name': 'Cool event',
+            'participants': ['Bob', 'Kim'],
+            'purchases': [{'purchaser': 'Bob', 'title': 'Gas', 'amount': 10, 'participants': [], 'description': None}]
+        }
+        self.assertDictEqual(expected_event, event.serialize())
