ae1927c118a2d6b3b0362127f4e84d1784ff6953
Vincent Tertre <vincent.tertre@gmail.com>
2015-08-03 17:29:52 +0200

    validators refactoring

--- agoodfriendalwayspayshisdebts/validators.py
+++ agoodfriendalwayspayshisdebts/validators.py
@@ -6,7 +6,6 @@ import commands
 class Validator(BusSynchronization):
     def __init__(self, message_type):
         super(Validator, self).__init__(message_type)
-        self.errors = []
 
 
 class CreateEventCommandValidator(Validator):
@@ -17,28 +16,33 @@ class CreateEventCommandValidator(Validator):
         self.__validate(command)
 
     def __validate(self, command):
+        violations = []
         if not command.name:
-            self.errors.append('EVENT_NAME_REQUIRED')
+            violations.append('EVENT_NAME_REQUIRED')
         if not command.participants or len(command.participants) < 1:
-            self.errors.append('PARTICIPANTS_REQUIRED')
+            violations.append('PARTICIPANTS_REQUIRED')
         else:
-            self.__validate_participants(command.participants)
-        if len(self.errors) > 0:
-            raise errors.ValidationError(self.errors)
+            violations.extend(self.__validate_participants(command.participants))
 
-    def __validate_participants(self, participants):
-        validation_errors = set([])
+        if len(violations) > 0:
+            raise errors.ValidationError(violations)
+
+    @staticmethod
+    def __validate_participants(participants):
+        participants_violations = set([])
+
         for participant in participants:
             if 'name' not in participant or not participant['name']:
-                validation_errors.add('PARTICIPANT_NAME_REQUIRED')
+                participants_violations.add('PARTICIPANT_NAME_REQUIRED')
             if 'share' not in participant or not participant['share']:
-                validation_errors.add('PARTICIPANT_SHARE_REQUIRED')
+                participants_violations.add('PARTICIPANT_SHARE_REQUIRED')
             else:
                 if participant['share'] < 1:
-                    validation_errors.add('INVALID_SHARE')
-            if len(validation_errors) == 3:
+                    participants_violations.add('INVALID_SHARE')
+            if len(participants_violations) == 3:
                 break
-        self.errors.extend(validation_errors)
+
+        return participants_violations
 
 
 class AddPurchaseCommandValidator(Validator):
@@ -48,15 +52,19 @@ class AddPurchaseCommandValidator(Validator):
     def before_execution(self, command):
         self.__validate(command)
 
-    def __validate(self, command):
+    @staticmethod
+    def __validate(command):
+        violations = []
+
         if not command.purchaser_id:
-            self.errors.append('PURCHASE_PURCHASER_REQUIRED')
+            violations.append('PURCHASE_PURCHASER_REQUIRED')
         if not command.label:
-            self.errors.append('PURCHASE_LABEL_REQUIRED')
+            violations.append('PURCHASE_LABEL_REQUIRED')
         if command.amount is None:
-            self.errors.append('PURCHASE_AMOUNT_REQUIRED')
+            violations.append('PURCHASE_AMOUNT_REQUIRED')
         else:
             if command.amount <= 0:
-                self.errors.append('INVALID_AMOUNT')
-        if len(self.errors) > 0:
-            raise errors.ValidationError(self.errors)
+                violations.append('INVALID_AMOUNT')
+
+        if len(violations) > 0:
+            raise errors.ValidationError(violations)
