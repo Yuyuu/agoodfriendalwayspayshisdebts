9ffbfa6383fcfb9015cea32c31e0efbab97508f6
Vincent Tertre <vincent.tertre@gmail.com>
2015-09-20 16:14:12 +0200

    Only send a reminder to the given recipients

--- src/main/java/agoodfriendalwayspayshisdebts/command/email/SendReminderCommandHandler.java
+++ src/main/java/agoodfriendalwayspayshisdebts/command/email/SendReminderCommandHandler.java
@@ -5,6 +5,8 @@ import agoodfriendalwayspayshisdebts.model.RepositoryLocator;
 import agoodfriendalwayspayshisdebts.model.event.Event;
 import com.vter.command.CommandHandler;
 
+import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 public class SendReminderCommandHandler implements CommandHandler<SendReminderCommand, Iterable<RecipientReport>> {
@@ -13,8 +15,15 @@ public class SendReminderCommandHandler implements CommandHandler<SendReminderCo
   public Iterable<RecipientReport> execute(SendReminderCommand command) {
     final Event event = RepositoryLocator.events().get(command.eventId);
 
+    final List<UUID> recipientsIds = toUuids(command.recipientsUuids);
+
     return event.participants().stream()
+        .filter(participant -> recipientsIds.contains(participant.id()))
         .map(participant -> RecipientReport.success(participant.name()))
         .collect(Collectors.toList());
+  }
+
+  private static List<UUID> toUuids(List<String> stringifiedUuids) {
+    return stringifiedUuids.stream().map(UUID::fromString).collect(Collectors.toList());
   }
 }
--- src/test/groovy/agoodfriendalwayspayshisdebts/command/email/SendReminderCommandHandlerTest.groovy
+++ src/test/groovy/agoodfriendalwayspayshisdebts/command/email/SendReminderCommandHandlerTest.groovy
@@ -11,24 +11,35 @@ class SendReminderCommandHandlerTest extends Specification {
   @Rule
   WithMemoryRepository repository = new WithMemoryRepository()
 
+  Participant lea = new Participant("lea", 1, "lea@email.com")
   Participant ben = new Participant("ben", 1, "ben@email.com")
-  Event event = new Event("", [ben])
+  String strLeaId = lea.id().toString()
+  String strBenId = ben.id().toString()
+  Event event = new Event("", [lea, ben])
 
   void setup() {
     RepositoryLocator.events().save(event)
   }
 
   def "generates fake reports"() {
-    given:
-    def strBenId = ben.id().toString()
+    when:
+    def command = new SendReminderCommand(eventId: event.id, recipientsUuids: [strLeaId, strBenId])
+    def reports = new SendReminderCommandHandler().execute(command)
 
+    then:
+    reports.size() == 2
+    def report = reports.first()
+    report.recipientName == "lea"
+    report.success
+  }
+
+  def "only send reminders to the given recipients"() {
     when:
     def command = new SendReminderCommand(eventId: event.id, recipientsUuids: [strBenId])
     def reports = new SendReminderCommandHandler().execute(command)
 
     then:
-    def report = reports.first()
-    report.recipientName == "ben"
-    report.success
+    reports.size() == 1
+    reports[0].recipientName == "ben"
   }
 }
