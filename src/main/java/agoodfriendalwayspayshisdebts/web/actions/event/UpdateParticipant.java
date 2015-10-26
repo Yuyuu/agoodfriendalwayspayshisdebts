2d6c0043b2f09ebfa53558fa33a116db460c0395
Vincent Tertre <vincent.tertre@gmail.com>
2015-10-26 18:30:54 +0100

    Update participant action

--- src/main/java/agoodfriendalwayspayshisdebts/web/actions/event/UpdateParticipant.java
+++ src/main/java/agoodfriendalwayspayshisdebts/web/actions/event/UpdateParticipant.java
@@ -1,14 +1,36 @@
 package agoodfriendalwayspayshisdebts.web.actions.event;
 
+import agoodfriendalwayspayshisdebts.command.event.UpdateParticipantCommand;
+import com.google.common.base.Throwables;
+import com.vter.command.CommandBus;
+import com.vter.infrastructure.bus.ExecutionResult;
 import net.codestory.http.annotations.Put;
 import net.codestory.http.annotations.Resource;
 import net.codestory.http.payload.Payload;
 
+import javax.inject.Inject;
+import java.util.UUID;
+
 @Resource
 public class UpdateParticipant {
 
+  @Inject
+  public UpdateParticipant(CommandBus commandBus) {
+    this.commandBus = commandBus;
+  }
+
   @Put("/events/:stringifiedEventUuid/participants/:stringifiedParticipantUuid")
-  public Payload update(String stringifiedEventUuid, String stringifiedParticipantUuid) {
+  public Payload update(
+      String stringifiedEventUuid, String stringifiedParticipantUuid, UpdateParticipantCommand command) {
+    command.eventId = UUID.fromString(stringifiedEventUuid);
+    command.id = UUID.fromString(stringifiedParticipantUuid);
+
+    final ExecutionResult<Void> result = commandBus.sendAndWaitResponse(command);
+    if (!result.isSuccess()) {
+      Throwables.propagate(result.error());
+    }
     return Payload.ok();
   }
+
+  private final CommandBus commandBus;
 }
--- src/test/groovy/agoodfriendalwayspayshisdebts/web/actions/event/UpdateParticipantTest.groovy
+++ src/test/groovy/agoodfriendalwayspayshisdebts/web/actions/event/UpdateParticipantTest.groovy
@@ -1,19 +1,41 @@
 package agoodfriendalwayspayshisdebts.web.actions.event
 
+import agoodfriendalwayspayshisdebts.command.event.UpdateParticipantCommand
+import com.vter.command.CommandBus
+import com.vter.infrastructure.bus.ExecutionResult
 import spock.lang.Specification
 
 class UpdateParticipantTest extends Specification {
+  CommandBus bus = Mock(CommandBus)
+
   UpdateParticipant action
 
   def setup() {
-    action = new UpdateParticipant()
+    action = new UpdateParticipant(bus)
   }
 
-  def "pretends the participant has been updated"() {
+  def "puts the command in the bus"() {
+    given:
+    def command = Mock(UpdateParticipantCommand)
+    bus.sendAndWaitResponse(command) >> ExecutionResult.success(null)
+
     when:
-    def payload = action.update(null, null)
+    def payload = action.update(UUID.randomUUID().toString(), UUID.randomUUID().toString(), command)
 
     then:
     payload.code() == 200
+  }
+
+  def "propagates the error if any occurred"() {
+    given:
+    def command = Mock(UpdateParticipantCommand)
+    bus.sendAndWaitResponse(command) >> ExecutionResult.error(new RuntimeException("error"))
+
+    when:
+    action.update(UUID.randomUUID().toString(), UUID.randomUUID().toString(), command)
+
+    then:
+    def error = thrown(RuntimeException)
+    error.message == "error"
   }
 }
